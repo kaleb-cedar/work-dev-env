c3c72bbe2deeb57383170306fa77d2b6c1bb8281
Kaleb Allmon <kallmon@cedar.com>
2025-01-17 14:56:05 -0500

    set ngrok provider domain

---
+++ aliases/set-ngrok-provider-domain.zsh
@@ -0,0 +1,5 @@
+function set-ngrok-provider-domain() {
+	cmd_str="UPDATE provider_domain SET domain = 'devtest2.cedar.com' WHERE id = 37;"
+
+	docker compose exec web bash -c "echo \"$cmd_str\" | ./manage.py dbshell"
+} 
