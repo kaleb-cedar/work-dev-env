08ec9cc7d0926e760704a64da78c17cdd59cfb52
Kaleb Allmon <kallmon@cedar.com>
2025-02-25 10:08:46 -0500

    add payment history ui enabled to command autocomplete

--- aliases/set-model-setting-value.zsh
+++ aliases/set-model-setting-value.zsh
@@ -26,6 +26,7 @@ function _set-model-setting-value_completion() {
 		"pre_visit__pre_visit_checkout_v2_ui_enabled"
 		"customization_editor_enabled"
 		"hide_balance_of_invoices_in_collections"
+		"payment_history_ui_enabled"
 	)
 
 	case "$words[2]" in
@@ -33,7 +34,8 @@ function _set-model-setting-value_completion() {
 		enable_nextjs_pcw_checkout_flow|\
 		pre_visit__pre_visit_checkout_v2_ui_enabled|\
 		customization_editor_enabled|\
-		hide_balance_of_invoices_in_collections)
+		hide_balance_of_invoices_in_collections|\
+		payment_history_ui_enabled)
 	    		local -a second_arg_options=("true" "false")
 	    	;;
 	  	show_new_york_debt_protection_disclaimer_option)
