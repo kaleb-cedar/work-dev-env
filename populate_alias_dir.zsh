1b7f794e919cb59769ddd850328a970c020692a5
Kaleb Allmon <kallmon@cedar.com>
2025-04-01 15:02:31 -0400

    only populate alias dir if changes detected

--- populate_alias_dir.zsh
+++ populate_alias_dir.zsh
@@ -5,5 +5,11 @@ ZSHRCS_ALIASES_DIR="$HOME/.zshrcs/src/aliases"
 
 mkdir -p "$ZSHRCS_ALIASES_DIR"
 for FILE in "$(dirname $(realpath "$0"))/aliases"/*; do
-    cp -i "$FILE" "$ZSHRCS_ALIASES_DIR"
+	if diff -q "$FILE" "$ZSHRCS_ALIASES_DIR/$FILE:t" >/dev/null; then
+		echo "\e[1;32m$FILE:t already exists in the same state in your configuration directory.\e[0m"	
+	else
+		printf "\e[1;33m"
+		cp -i "$FILE" "$ZSHRCS_ALIASES_DIR"
+		printf "\e[0m"
+	fi
 done
