62bb81173c539ba295aa100f0964c68ca62aab2f
Kaleb Allmon <kallmon@cedar.com>
2025-04-01 13:06:17 -0400

    built in update snapshot

--- aliases/django-test.zsh
+++ aliases/django-test.zsh
@@ -1,11 +1,32 @@
 #!/usr/bin/env zsh
 
 function django-test() {
-	testpath=${1:-"."}
-	docker compose exec web bash -c "./manage.py test $testpath"
+	zparseopts -D -E u=u_flag
+
+	if [[ -z "$1" ]]; then
+	  echo "Error: Missing required argument."
+	  echo "Usage: $0 <django-project> [path]"
+	  return 1
+	fi
+
+	testpath=${2:-"."}
+	if (( $#u_flag )); then
+		docker compose exec web bash -c "cd ../$1 && ./manage.py test $testpath --snapshot-update"
+	else
+		docker compose exec web bash -c "cd ../$1 && ./manage.py test $testpath"
+	fi
 }
 
-function django-test-update-snapshot() {
-	testpath=${1:-"."}
-	docker compose exec web bash -c "./manage.py test $testpath --snapshot-update"
+function _django-test_completion() {
+	local -a first_arg_options=(
+		"api"
+		"integrations"
+	)
+
+  	if (( CURRENT == 2 )); then
+    		compadd "${first_arg_options[@]}"
+  	fi
 }
+
+compdef _django-test_completion django-test
+
